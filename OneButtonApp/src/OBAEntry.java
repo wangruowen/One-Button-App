fa8b1dd041a091a6be60afa58156881f622b3a48
Bharath <bharathurh@gmail.com>
2012-04-08 18:08:08 -0300

    Update OneButtonApp/src/OBAEntry.java

--- OneButtonApp/src/OBAEntry.java
+++ OneButtonApp/src/OBAEntry.java
@@ -7,23 +7,14 @@ public class OBAEntry {
 	}
 
 	public int getImageID(int index) {
-		if(image_list.length!=0)
 		return image_list[index].imgID;
-		else
-		return null;
 	}
 
 	public String getImageName(int index) {
-		if(image_list.length!=0)
 		return image_list[index].imgName;
-		else
-		return null;
 	}
 
 	public String getImageDesc(int index) {
-		if(image_list.length!=0)
 		return image_list[index].imgDesc;
-		else
-		return null;
 	}
 }
