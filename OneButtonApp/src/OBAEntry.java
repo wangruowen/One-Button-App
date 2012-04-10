1500a8456563e94bb41f044c96ecddfa0ef50c62
wangruowen <wangruowen@gmail.com>
2012-04-10 17:28:06 -0400

    Only modify the OBAEntry class, adding the "load_mode" to indicate whether the target OBA is using SSH/RDP to login. For the first time, login, we have to try both to see which one to choose. Later, as this login_mode is stored in the DB, we can directly login.

--- OneButtonApp/src/OBAEntry.java
+++ OneButtonApp/src/OBAEntry.java
@@ -1,17 +1,33 @@
 public class OBAEntry {
+	private int image_id;
+	private String image_name;
+	private String image_desc;
+	private int load_mode;
 
-	public String getImageID() {
+	public OBAEntry(int image_id, String image_name, String image_desc,
+			int load_mode) {
+		this.image_id = image_id;
+		this.image_name = image_name;
+		this.image_desc = image_desc;
+		this.load_mode = load_mode;
+	}
+
+	public int getImageID() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.image_id;
 	}
 
 	public String getImageName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.image_name;
 	}
 
 	public String getImageDesc() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.image_desc;
+	}
+
+	public int getLoadMode() {
+		return this.load_mode;
 	}
 }
