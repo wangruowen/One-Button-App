8f4eab66772bf1ffe07be8eda15f37979ad0e780
wangruowen <wangruowen@gmail.com>
2012-04-10 18:29:00 -0300

    Update OneButtonApp/src/OBAEntry.java

--- OneButtonApp/src/OBAEntry.java
+++ OneButtonApp/src/OBAEntry.java
@@ -1,20 +1,33 @@
 public class OBAEntry {
-	
-	public int getImageInfo(String userName)
-	{
-		image image_list[]=imageManager.getData(userName);
-		return image_list.length;
+	private int image_id;
+	private String image_name;
+	private String image_desc;
+	private int load_mode;
+
+	public OBAEntry(int image_id, String image_name, String image_desc,
+			int load_mode) {
+		this.image_id = image_id;
+		this.image_name = image_name;
+		this.image_desc = image_desc;
+		this.load_mode = load_mode;
 	}
 
-	public int getImageID(int index) {
-		return image_list[index].imgID;
+	public int getImageID() {
+		// TODO Auto-generated method stub
+		return this.image_id;
 	}
 
-	public String getImageName(int index) {
-		return image_list[index].imgName;
+	public String getImageName() {
+		// TODO Auto-generated method stub
+		return this.image_name;
 	}
 
-	public String getImageDesc(int index) {
-		return image_list[index].imgDesc;
+	public String getImageDesc() {
+		// TODO Auto-generated method stub
+		return this.image_desc;
+	}
+
+	public int getLoadMode() {
+		return this.load_mode;
 	}
 }
